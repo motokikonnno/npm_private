7902f595c89e0a6883e9f9e25155b02f420f955c
motoki <motoki610972@icloud.com>
2023-06-23 14:25:31 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export const fetchJsonPlaceholder = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+  const data = await res.json();
+
+  console.log(data);
+};
